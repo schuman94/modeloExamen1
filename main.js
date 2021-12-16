13848d24fadd753d3bc5c5036c6472b769988de6
sergio <sergio.chulian@iesdonana.org>
2021-12-16 20:30:06 +0100

    segundo

--- main.js
+++ main.js
@@ -13,24 +13,38 @@ function Usuario(nombre, email, dni, rango) {
 function modoInvitado() {
     modo = "invitado";
     usuario = new Usuario("", "", "", "invitado");
+    eliminarBoton("logout");
+    eliminarBoton("guardar");
+    eliminarBoton("administrador");
+    if (!document.getElementById("login")) {
+        crearBoton("login", "Login", modoUsuario);
+    }
 }
 
 function modoUsuario() {
     modo = "usuario";
     eliminarBoton("login");
+    crearBoton("logout", "LogOut", modoInvitado);
     crearBoton("guardar", "Guardar Datos");
-    //crearBoton("administrador");
+    document.getElementById("guardar").setAttribute("disabled", "");
+    //document.getElementById("guardar").removeAttribute("disabled"); Esto lo haremos cuando validemos el formulario
+    crearBoton("administrador", "Modo Administrador");
 }
 
 function modoAdministrador() {
     modo = "administrador"
 }
 
-function crearBoton(id, mensaje) {
+function crearBoton(id, mensaje, funcion) {
     let boton = document.createElement("button");
     boton.setAttribute("id", id);
     boton.textContent = mensaje;
     document.getElementById("contenedorUno").appendChild(boton);
+    darFuncion(id, funcion);
+}
+
+function darFuncion(id, funcion) {
+    document.getElementById(id).addEventListener("click", funcion);
 }
 
 function eliminarBoton(id) {
@@ -43,10 +57,11 @@ function eliminarBoton(id) {
     }
 }
 
-document.getElementById("login").addEventListener("click", modoUsuario);
+
 
 
 
 
 // Inicio del html
 modoInvitado();
+document.getElementById("login").addEventListener("click", modoUsuario);
